0130371130ff4146e0da9e0c498470fd608106ea
Niko Raes <niko.raes@arcadis.com>
2024-03-18 07:24:40 +0100

    WIP

--- exts/dtdl.property/dtdl/property/dtdl_model_modelrepo.py
+++ exts/dtdl.property/dtdl/property/dtdl_model_modelrepo.py
@@ -1,5 +1,7 @@
 from typing import List
 
+from omni.kit.property.usd.usd_property_widget import UsdPropertyUiEntry
+
 
 class DtdlProperty:
 
@@ -17,56 +19,13 @@ class DtdlProperty:
 
 
 class DtdlExtendedModelData:
-    def __init__(self, model: object):
+    def __init__(self, model: object, all_models: list[object]):
         self.model = model
+        self.bases: List[str] = []
+        self.properties: List[UsdPropertyUiEntry] = []
 
-    bases: List[str] = []
-    properties: List[DtdlProperty] = []
-
-
-"""  // recursively add all properties (also from parent models)
-  const addModelContents = (model: DtdlInterface, reset = true) => {
-    // reset category order
-    if (reset) resetCategoryOrder()
-    if (model.contents) {
-      // workaround for model definition with just 1 content defined as object instead of array
-      if (!Array.isArray(model.contents) && typeof model.contents === "object") model.contents = [model.contents]
-      contents.properties.unshift(...model.contents
-        .filter((c): c is DtdlProperty => (c["@type"] === "Property" || c["@type"].includes("Property")) && !contents.properties.some(p => p.name === c.name))
-        .map(p => replaceUriKeys(p)))
-      // sort properties by commentOrder
-      contents.properties.sort(compareCommentOrder)
-      contents.relationships.unshift(...model.contents
-        .filter((c): c is DtdlRelationship => c["@type"] === "Relationship" && !contents.relationships.some(p => p.name === c.name))
-        .map(p => replaceUriKeys(p)))
-      contents.telemetries.unshift(...model.contents
-        .filter((c): c is DtdlTelemetry => (c["@type"] === "Telemetry" || c["@type"].includes("Telemetry")) && !contents.telemetries.some(p => p.name === c.name))
-        .map(p => replaceUriKeys(p)))
-      contents.components.unshift(...model.contents
-        .filter((c): c is DtdlComponent => c["@type"] === "Component" && !contents.components.some(p => p.name === c.name))
-        .map(p => replaceUriKeys(p)))
-    }
-    if (model.extends) {
-      if (Array.isArray(model.extends)) {
-        model.extends.forEach(x => {
-          contents.bases.unshift(x)
-          const baseModel = models.find(y => y.id === x)?.model
-          if (baseModel) addModelContents(baseModel, false)
-        })
-      } else {
-        contents.bases.unshift(model.extends)
-        const baseModel = models.find(y => y.id === model.extends)?.model
-        if (baseModel) addModelContents(baseModel, false)
-      }
-    }
-  } """
-
-
-class DtdlModelRepository:
-    def __init__(self, models: List[object]):
-        self._model_repo: dict[str, DtdlExtendedModelData] = []
-
-    def _add_model_contents_recursive(self, model: object):
+    def _add_model_contents_recursive(self, model_id: str, all_models: list[object]):
+        model = next(m for m in all_models if m["@id"] == model_id)
         self._model_repo[model["@id"]] = {["model"]: model, ["properties"]: []}
         if "contents" in model:
             if model["contents"] is object:
@@ -74,12 +33,32 @@ class DtdlModelRepository:
             for c in model["contents"]:
                 if c["@type"] == "Property" or "Property" in c["@type"]:
                     self._model_repo[model["@id"]]["properties"].append(c)
+        if "extends" in model:
+            if isinstance(model["extends"], list):
+                for base in model["extends"]:
+                    self._add_model_contents_recursive(base, all_models)
+            else:
+                self._add_model_contents_recursive(model["extends"], all_models)
 
-    def get_model(self, model_id: str) -> DtdlModel:
-        return self._model_repo.get_model(model_id)
+    def _usd_property_from_dtdl_property(
+        self, dtdl_property: object, modelId: str
+    ) -> UsdPropertyUiEntry:
+        id: str = dtdl_property["@id"] if "@id" in dtdl_property else ""
+        return UsdPropertyUiEntry(
+            "dtmi:com:arcadis:test:boolean",
+            "",
+            {
+                Sdf.PrimSpec.TypeNameKey: "bool",
+                "customData": {"default": False},
+            },
+            Usd.Attribute,
+        )
 
-    def add_model(self, model: DtdlModel):
-        self._model_repo.add_model(model)
 
-    def get_properties(self, model_id: str) -> List[DtdlProperty]:
-        return self._model_repo.get_properties(model_id)
+class DtdlModelRepository:
+    """
+    Stores the USD attribute definitions per DTDL model for easy retrieval
+    """
+
+    def __init__(self, models: list[object]):
+        self._model_repo: dict[str, list[UsdPropertyUiEntry]] = []
